3570d8ba0506b587037846f679e8ef8c6a08c913
Deepali Chourasia <deepch23@in.ibm.com>
2022-06-22 11:44:17 +0000

    add patch to avoid looking for torch through cmake

--- text-0.13/recipe/build.sh
+++ text-0.13/recipe/build.sh
@@ -20,21 +20,8 @@ set -ex
 export CXXFLAGS="$(echo ${CXXFLAGS} | sed -e 's/ -std=[^ ]*//')"
 export CXXFLAGS="${CXXFLAGS} -D__STDC_FORMAT_MACROS"
 
-if [[ $build_type == "cuda" ]]
-then
-  export CMAKE_CUDA_HOST_COMPILER=$GXX
-  export CMAKE_CUDA_COMPILER=${CUDA_HOME}/bin/nvcc
-  export CMAKE_CUDA_ARCHITECTURES="5.2 6.0 6.1 7.0 7.5 8.0 8.6+PTX"
-  export FORCE_CUDA=1
-
-  # Create symlinks of cublas headers into CONDA_PREFIX
-  mkdir -p $CONDA_PREFIX/include
-  find /usr/include -name cublas*.h -exec ln -s "{}" "$CONDA_PREFIX/include/" ';'
-  export CUDA_INCLUDE_PATH="${PREFIX}/include,${CUDA_HOME}/include,${CONDA_PREFIX}/include"
-fi
-
 PYTHON_VERSION=$(python -V 2>&1 | cut -d ' ' -f 2 | cut -d '.' -f 1,2)
 PACKAGE_PATH=$PREFIX/lib/python${PYTHON_VERSION}/site-packages/
+export TORCH_INSTALL_PREFIX=$PACKAGE_PATH/torch/
 
 python setup.py install --single-version-externally-managed --record=record.txt
-#cmake .
